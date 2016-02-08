28c7e1bd4ffabf50ee1550aaa2ac1a3ecc0cae9f
Coira <coira.ells@gmail.com>
2016-02-08 16:05:35 +0000

    Fetches news feed. Lays out news items. Needs styling

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ajax from 'superagent';
 
 class Card extends React.Component {
-    render() {
-	return (
-	    <div>
-	    <p>This is a card</p>
-	    </div>
-	);
+    constructor(props) {
+	super(props);
+	this.state = { news: [] }
     }
+    
+    componentWillMount() {
+	const baseURL = 'http://www.freecodecamp.com/news/hot';
+	ajax.get('http://www.freecodecamp.com/news/hot').
+	     end((error, response) => {
+		 if (!error && response) {
+		     this.setState({news: response.body});
+		 }
+		 else {
+		     console.log('There was an error fetching the feed from freecodecamp', error);
+		 }
+	     });
+    }
+    
+    render() {
+	return (<div>
+	    <h1>Camper News</h1>
+	    <div className = 'cardContainer'>
+	    {this.state.news.map((newsItem, index) => (
+		<div key={index} className='card'>
+		    <p> {newsItem.headline} </p>
+		    <p> {newsItem.author.username} </p>
+		</div>
+	    ))}
+	</div>
+	</div>);
+    }
+
 }
+
 
 ReactDOM.render(
     <Card />,
